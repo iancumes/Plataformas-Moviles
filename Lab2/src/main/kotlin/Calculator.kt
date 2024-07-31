5c99b10aa0e4d07eaea8e343642416d29259c394
Ian Cumes <ianrodrigocumes@gmail.com>
2024-07-30 22:52:59 -0600

    Preparacion del programa

---
+++ Lab2/src/main/kotlin/Calculator.kt
@@ -0,0 +1,8 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.math.exp
+import java.util.Stack
+
+
+object Calculator {
+}
---
+++ Lab2/src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,7 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import java.util.EmptyStackException
+import kotlin.math.exp
+import java.util.Stack
+class CalculatorTest {
+}
