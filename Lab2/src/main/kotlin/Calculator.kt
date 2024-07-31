113b558f86cc7acb1c4cc84adb1d0ca8351614af
Ian Cumes <ianrodrigocumes@gmail.com>
2024-07-30 23:01:01 -0600

    Infix to postfix

--- Lab2/src/main/kotlin/Calculator.kt
+++ Lab2/src/main/kotlin/Calculator.kt
@@ -32,3 +32,41 @@ object Calculator {
 
 
     }
+
+fun infixToPostfix(expression: String): String {
+    val result = StringBuilder()
+    val stack = Stack<Char>()
+    var i = 0
+    while (i < expression.length) {
+        val c = expression[i]
+
+        // If the character is an operand, add it to output
+        if (c.isDigit() || c == '.') {
+            while (i < expression.length && (expression[i].isDigit() || expression[i] == '.')) {
+                result.append(expression[i++])
+            }
+            result.append(' ')
+            i--
+        } else if (c == '(') {
+            stack.push(c)
+        } else if (c == ')') {
+            while (stack.isNotEmpty() && stack.peek() != '(') {
+                result.append(stack.pop()).append(' ')
+            }
+            stack.pop()
+        } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == 'e' || c == 'r') {
+            while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                result.append(stack.pop()).append(' ')
+            }
+            stack.push(c)
+        }
+        i++
+    }
+
+    // Pop all the operators from the stack
+    while (stack.isNotEmpty()) {
+        result.append(stack.pop()).append(' ')
+    }
+
+    return result.toString()
+}
