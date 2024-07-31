cd0b7a5bdeeb016f6e3afb4afae8739bfc6e970f
Javier Valladares <javivalladares10@icloud.com>
2024-07-30 22:59:33 -0600

    test mult

--- Lab2/src/test/kotlin/CalculatorTest.kt
+++ Lab2/src/test/kotlin/CalculatorTest.kt
@@ -14,6 +14,11 @@ class CalculatorTest {
         val result = Calculator.evaluate("10 - 2")
         assertEquals(8.0, result)
     }
+    @Test
+    fun testMultiplication() {
+        val result = Calculator.evaluate("4 * 2")
+        assertEquals(8.0, result)
+    }
 
 
 }
