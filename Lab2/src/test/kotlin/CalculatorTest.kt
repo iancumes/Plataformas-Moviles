fd7c7b323ddff8076402b009d901c5870bfc1437
Javier Valladares <javivalladares10@icloud.com>
2024-07-30 22:58:24 -0600

    test suma

--- Lab2/src/test/kotlin/CalculatorTest.kt
+++ Lab2/src/test/kotlin/CalculatorTest.kt
@@ -4,4 +4,10 @@ import java.util.EmptyStackException
 import kotlin.math.exp
 import java.util.Stack
 class CalculatorTest {
+    @Test
+    fun testAddition() {
+        val result = Calculator.evaluate("3 + 5")
+        assertEquals(8.0, result)
+    }
+
 }
