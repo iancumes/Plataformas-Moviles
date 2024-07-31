972f0aa5ebd0b292a0ab975a4ef3cc469fcd657d
Javier Valladares <javivalladares10@icloud.com>
2024-07-30 22:56:27 -0600

    Funcion precedence

--- Lab2/src/main/kotlin/Calculator.kt
+++ Lab2/src/main/kotlin/Calculator.kt
@@ -5,4 +5,17 @@ import java.util.Stack
 
 
 object Calculator {
-}
+    // Function to check the precedence of operators
+    private fun precedence(op: Char): Int {
+        return when (op) {
+            '+', '-' -> 1
+            '*', '/' -> 2
+            '^', 'r' -> 3
+            'e' -> 4
+            else -> -1
+        }
+    }
+
+
+
+    }
